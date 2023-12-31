6f43a01e6b501383b2d215512c2b03ddd6c77d33
stuart <pkplonker@gmail.com>
2023-12-31 17:41:58 +0000

    Task: Draw grid

--- Editor/Source/Windows/DebugControls.cpp
+++ Editor/Source/Windows/DebugControls.cpp
@@ -5,26 +5,53 @@
 #include "scene.h"
 #include "../DebugDrawer.h"
 #include "Implementation/Scene.h"
+#include "../EditorSettings.h"
+
+void DebugControls::DrawBounds()
+{
+    if (const auto scene = SceneManager::GetScene().lock())
+    {
+        for (auto [GUID, gameObject] : scene->GetObjects())
+        {
+            if (!gameObject->GetIsEnabled())continue;
+            const auto& bounds = gameObject->GetAABB();
+            DebugDrawer::DrawBoundingBox(bounds);
+        }
+    }
+}
+
+DebugControls::DebugControls()
+{
+    drawBounds = EditorSettings::Get(drawBoundsKey, true);
+    drawGrid = EditorSettings::Get(drawGridKey, true);
+}
+
+DebugControls::~DebugControls()
+{
+    EditorSettings::Set(drawBoundsKey, drawBounds);
+    EditorSettings::Set(drawGridKey, drawGrid);
+}
 
 void DebugControls::Draw()
 {
     ImGui::Begin(DEBUG_CONTROLS.c_str());
-    static bool drawBounds = true;
+
     if (ImGui::Checkbox("Draw Bounds", &drawBounds))
     {
     }
+    ImGui::SameLine();
+    if (ImGui::Checkbox("Draw Grid", &drawGrid))
+    {
+    }
+
+    DebugDrawer::DrawGrid(drawGrid);
+
     if (drawBounds)
     {
-        if (const auto scene = SceneManager::GetScene().lock())
-        {
-            for (auto [GUID, gameObject] : scene->GetObjects())
-            {
-                if (!gameObject->GetIsEnabled())continue;
-                const auto& bounds = gameObject->GetAABB();
-                DebugDrawer::DrawBoundingBox(bounds);
-            }
-        }
+        DrawBounds();
     }
+
+
     ImGui::End();
 }
 
--- Editor/Source/Windows/DebugControls.h
+++ Editor/Source/Windows/DebugControls.h
@@ -7,6 +7,16 @@ const std::string DEBUG_CONTROLS = "Debug Controls";
 class DebugControls : public EditorWindow
 {
 public:
+    DebugControls();
+    ~DebugControls() override;
     void Draw() override;
     std::string GetName() override;
+
+private:
+    void DrawBounds();
+    bool drawBounds = false;
+    bool drawGrid = false;
+    std::string drawBoundsKey ="DEBUG_DRAW_BOUNDS";
+    std::string drawGridKey ="DEBUG_DRAW_GRID";
+
 };
